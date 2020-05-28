8ff20e25710bf804cd6c741a1fab78ac43c11dc5
Ivan <gorden2001@mail.ru>
2020-05-28 13:38:50 +0300

    Программа доработана.

--- Tests/MainTest.java
+++ Tests/MainTest.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.Test;
 
+import java.rmi.UnexpectedException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
-    public void ShowTree() {
+    public void GetCheck() {
         Tree tree = new Tree();
         tree.AddToTree(new Item(6, 7));
         tree.AddToTree(new Item(4, 9));
@@ -13,6 +15,21 @@ class MainTest {
         tree.AddToTree(new Item(2, 20));
         tree.AddToTree(new Item(7, 2));
         tree.AddToTree(new Item(5, 4));
-        assertEquals(tree.get(6),7);
+        assertEquals(tree.get(6), 7);
+    }
+
+    @Test
+    public void DeleteCheck() {
+        Tree tree = new Tree();
+        tree.AddToTree(new Item(6, 7));
+        tree.AddToTree(new Item(4, 9));
+        tree.AddToTree(new Item(10, 11));
+        tree.AddToTree(new Item(12, 18));
+        tree.AddToTree(new Item(2, 20));
+        tree.AddToTree(new Item(7, 2));
+        tree.AddToTree(new Item(5, 4));
+        tree.DeleteFromTree(7);
+        System.out.println("Если вылезла ошибка NullPointerException,то тест пройден");
+        tree.get(7);
     }
 }
--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -20,57 +20,14 @@ public class Tree {
         }
     }
 
-    public void DeleteFromTree(int key) {
-        if (size > 0) {
-            if (head.key == key) {
-                Delete(key, head);
-            } else if (head.key < key) {
-                FindRightDeleteElement(key, head.right);
-            } else
-                FindLeftDeleteElement(key, head.left);
-        }
-    }
-
-    public void Delete(int key, Item item) {
-        if ((item.right == null) && (item.left == null)) {
-            item = null;
-        } else if (item.right == null) {
-            item = item.left;
-        } else if (item.right.left == null) {
-            item = item.right;
-        } else FindAndDelete(item.right.left, item);
-    }
-
-    public void FindAndDelete(Item left, Item element) {
-        if (left.left == null) {
-            element = left;
-        } else FindAndDelete(left.left, element);
-    }
-
-    public void FindRightDeleteElement(int key, Item right) {
-        if (key == right.key) {
-            Delete(key, right);
-        } else if (right.key < key) {
-            FindRightDeleteElement(key, right.right);
-        } else
-            FindLeftDeleteElement(key, right.left);
-    }
-
-    public void FindLeftDeleteElement(int key, Item left) {
-        if (key == left.key) {
-            Delete(key,left);
-        } else if (left.key < key) {
-            FindRightDeleteElement(key, left.right);
-        } else
-            FindLeftDeleteElement(key, left.left);
-    }
-
     public void AddToLeft(Item left, Item item) {
         if ((item.key > left.key) && (left.right == null)) {
             left.right = item;
+            size++;
         } else {
             if ((item.key < left.key) && (left.left == null)) {
                 left.left = item;
+                size++;
             } else {
                 if (item.key > left.key) AddToRight(left.right, item);
                 else AddToLeft(left.left, item);
@@ -81,9 +38,11 @@ public class Tree {
     public void AddToRight(Item right, Item item) {
         if ((item.key > right.key) && (right.right == null)) {
             right.right = item;
+            size++;
         } else {
             if ((item.key < right.key) && (right.left == null)) {
                 right.left = item;
+                size++;
             } else {
                 if (item.key > right.key) AddToRight(right.right, item);
                 else AddToLeft(right.left, item);
@@ -92,33 +51,73 @@ public class Tree {
     }
 
     public int get(int key) {
-        if (size > 0) {
+        int allelementssize = size;
+        if (allelementssize > 0) {
             if (head.key == key) {
                 return head.value;
             } else if (head.key < key) {
-                return getRight(key, head.right);
+                allelementssize--;
+                return getRight(key, head.right, allelementssize);
             } else
-                return getLeft(key, head.left);
+                allelementssize--;
+            return getLeft(key, head.left, allelementssize);
         }
         return 0;
     }
 
 
-    public int getRight(int key, Item right) {
+    public int getRight(int key, Item right, int allelementssize) {
         if (key == right.key) {
             return right.value;
         } else if (right.key < key) {
-            return getRight(key, right.right);
+            allelementssize--;
+            return getRight(key, right.right, allelementssize);
         } else
-            return getLeft(key, right.left);
+            allelementssize--;
+        return getLeft(key, right.left, allelementssize);
     }
 
-    public int getLeft(int key, Item left) {
+    public int getLeft(int key, Item left, int allelementssize) {
         if (key == left.key) {
             return left.value;
         } else if (left.key < key) {
-            return getRight(key, left.right);
+            allelementssize--;
+            return getRight(key, left.right, allelementssize);
         } else
-            return getLeft(key, left.left);
+            allelementssize--;
+        return getLeft(key, left.left, allelementssize);
+    }
+
+    public void DeleteFromTree(int key) {
+        Item item1 = head;
+        Item item2 = null;
+        while (item1 != null) {
+            int difference = key - item1.key;
+            if (difference == 0) break;
+            else {
+                item2 = item1;
+                if (difference < 0)item1 = item1.left;
+                else item1 = item1.right;
+            }
+        }
+        if (item1 == null) return;
+        if (item1.right == null) {
+            if (item2 == null) head = item1.left;
+            else {
+                if (item1 == item2.left) item2.left = item1.left;
+                else item2.right = item1.left;
+            }
+        } else {
+            Item farleft = item1.right;
+            item2 = null;
+            while (farleft.left != null) {
+                item2 = farleft;
+                farleft = farleft.left;
+            }
+            if (item2 != null) item2.left = farleft.right;
+            else item1.right = farleft.right;
+            item1.key = farleft.key;
+            item1.value = farleft.value;
+        }
     }
 }
